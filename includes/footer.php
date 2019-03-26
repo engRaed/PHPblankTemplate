307c6ce3c06ca9a7579d95e24fd024a14172035a
engRaed <39016921+engRaed@users.noreply.github.com>
2019-03-26 06:05:40 +0300

    blank php template using bootstrap
    blank php template using bootstrap

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<script src="js/jquery-3.2.1.slim.min.js"></script>
+
+
+		<script src="js/popper.min.js"></script>
+
+		<script src="js/bootstrap_4.min.js"></script>
+
+		<link rel="stylesheet" href="css/mystyle.css">
+
+		
+
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php  include('includes/header.php');  ?>
+
+
+<div class="container">
+
+    <div class="row">
+			<div class="col-lg-10">
+
+					<div class="starter-template">
+						<h1>Bootstrap starter template</h1>
+						<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+					</div>
+
+		   </div>
+
+			<div class="col-lg-2  bg-info">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit ipsam commodi pariatur doloremque rerum quibusdam odit vel, a enim architecto placeat deserunt iusto laborum id voluptatum unde aliquam ex repellat voluptate nisi. Ducimus ab, illum nulla illo deleniti aliquid eum, veritatis aspernatur rerum natus. Quos, aut tempore ea sint sequi odit obcaecati itaque quisquam ad porro quam, maxime minus illo assumenda numquam laudantium. Esse, vero est quos nam nostrum ducimus harum, aliquam error voluptatum possimus repellendus sed eius, qui ad quas iure, porro iste enim neque nesciunt voluptas exercitationem voluptates corrupti. Incidunt ipsum fuga harum excepturi amet soluta porro beatae!
+				
+			   </div>
+
+</div>
+
+
+	
+
+</div><!-- /.container -->
+
+
+<?php include('includes/footer.php')  ?>
